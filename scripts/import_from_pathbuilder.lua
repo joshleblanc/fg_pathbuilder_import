3c6e35a671b98e68d9a615cf3e5064503f8f2e6c
FastChoker <fastchoker@gmail.com>
2023-02-04 23:57:12 -0500

    Updates to create a PC sheet, and set the name

--- scripts/import_from_pathbuilder.lua
+++ scripts/import_from_pathbuilder.lua
@@ -4,4 +4,12 @@ end
 
 function onInit()
     Comm.registerSlashHandler("importpathbuilder", importFromPathbuilder)
+    Comm.registerSlashHandler("impb", importFromPathbuilder)
+end
+
+function doPBImport()
+    local nodeTarget = DB.createChild("charsheet");
+    local nodeId = DB.getChild(nodeTarget, "name");
+    -- Test setting the name
+    DB.setValue(nodeTarget, "name", "string", "TESTER");
 end
