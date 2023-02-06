2bd2ed9ff452e2a51277ef66b8175344117dacc4
FastChoker <fastchoker@gmail.com>
2023-02-06 00:09:14 -0500

    Adding in JSON parsing

--- scripts/import_from_pathbuilder.lua
+++ scripts/import_from_pathbuilder.lua
@@ -7,13 +7,25 @@ function onInit()
     Comm.registerSlashHandler("impb", importFromPathbuilder)
 end
 
-function doPBImport()
+function doPBImport(pcJson)
+    
+    local jsonTable = JSONUtil.parseJson(pcJson);
+
     local nodeTarget = DB.createChild("charsheet");
     local nodeId = DB.getChild(nodeTarget, "name");
     -- Test setting the name
-    DB.setValue(nodeTarget, "name", "string", "TESTER");
-    local featName = "Unconventional Weaponry";
-    featName = string.lower(featName);
-    local featMatchNode = ManagerGetRefData.getFeat(featName);
-    CharManager.addFeat(nodeTarget, "referencefeat", featMatchNode.getNodeName());
+    local name = jsonTable["build"]["name"];
+    local ancestry = jsonTable["build"]["ancestry"];
+    local ancestryNode = ManagerGetRefData.getLookupDataRecordGlobally("reference.ancestries.poppet@Pathfinder 2 RPG - Lost Omens The Grand Bazaar", "referencerace");
+
+    DB.setValue(nodeTarget, "name", "string", name);
+    
+
+    --DB.setValue(nodeTarget, "race", "string", ancestry);
+    --CharManager.addRace(nodeTarget, "referencerace", ancestryNode);
+
+    --local featName = "Unconventional Weaponry";
+    --featName = string.lower(featName);
+    --local featMatchNode = ManagerGetRefData.getFeat(featName);
+    --CharManager.addFeat(nodeTarget, "referencefeat", featMatchNode.getNodeName());
 end
