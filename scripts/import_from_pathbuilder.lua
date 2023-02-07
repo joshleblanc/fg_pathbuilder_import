9b8781b2eb440281518edcf3195dd6369345afa3
FastChoker <fastchoker@gmail.com>
2023-02-06 23:33:03 -0500

    Now adds Ancestry, Background, and Class.

--- scripts/import_from_pathbuilder.lua
+++ scripts/import_from_pathbuilder.lua
@@ -7,22 +7,70 @@ function onInit()
     Comm.registerSlashHandler("impb", importFromPathbuilder)
 end
 
-function doPBImport(pcJson)
+function doPBImport(pcJson, importWindow)
     
     local jsonTable = JSONUtil.parseJson(pcJson);
 
-    local nodeTarget = DB.createChild("charsheet");
-    local nodeId = DB.getChild(nodeTarget, "name");
-    -- Test setting the name
+    local nodeChar = DB.createChild("charsheet");
+
+    -- Setting the name
     local name = jsonTable["build"]["name"];
+
+    DB.setValue(nodeChar, "name", "string", name);
+    
     local ancestry = jsonTable["build"]["ancestry"];
-    local ancestryNode = DB.findNode("poppet.racelink");
- 
-    DB.setValue(nodeTarget, "name", "string", name);
+    
+    -- List ancestries in modules
+    local recordNodes = DB.getChildrenGlobal("reference.ancestries"); 
+    for k, recordNode in pairs(recordNodes) do
+        local sRecordName = StringManager.trim(DB.getValue(recordNode, "name", ""));
+
+        if (sRecordName == ancestry) then
+            -- ancestry found, set the ancestry
+            DB.setValue(nodeChar, "race", "string", sRecordName);
+	        DB.setValue(nodeChar, "racelink", "windowreference", "referencerace", recordNode.getNodeName());
+            break;
+        end
+        --error('Unable to find the ancestry, make sure module is loaded');
+    
+    end
+
+    local background = jsonTable["build"]["background"];
+    local backgroundRecords = DB.getChildrenGlobal("reference.backgrounds");
+    for k, backgroundNode in pairs(backgroundRecords) do
+        local sRecordName = StringManager.trim(DB.getValue(backgroundNode, "name", ""));
+        --print('Record name: ' .. sRecordName);
+        if (sRecordName:lower() == background:lower()) then
+            -- background found, set the background
+            DB.setValue(nodeChar, "background", "string", sRecordName);
+	        DB.setValue(nodeChar, "backgroundlink", "windowreference","reference_background", backgroundNode.getNodeName());
+            break;
+        end
+        --error('Unable to find the background, make sure module is loaded');
+    
+    end
+
+    -- Class
+    local charClass = jsonTable["build"]["class"];
     
 
-    --DB.setValue(nodeTarget, "race", "string", ancestry);
-    --CharManager.addRace(nodeTarget, "referencerace", ancestryNode);
+    local classRecords = DB.getChildrenGlobal("reference.classes");
+    for k, classNode in pairs(classRecords) do
+        local sRecordName = StringManager.trim(DB.getValue(classNode, "name", ""));
+        --print('Record name: ' .. sRecordName);
+        if (sRecordName:lower() == charClass:lower()) then
+            -- class found, set the class (NOTE: Only doing 1 class now)
+            --CharManager.addClass(nodeChar, "referenceclass", classNode.getNodeName());
+            local nodeList = nodeChar.createChild("classes");
+            nodeClass = nodeList.createChild();
+            DB.setValue(nodeClass, "name", "string", sRecordName);
+            DB.setValue(nodeClass, "level", "number", 1); -- TODO: Read Level
+	        DB.setValue(nodeClass, "shortcut", "windowreference", "referenceclass", classNode.getNodeName());
+            break;
+        end
+        --error('Unable to find the background, make sure module is loaded');
+    
+    end
 
     --local featName = "Unconventional Weaponry";
     --featName = string.lower(featName);
