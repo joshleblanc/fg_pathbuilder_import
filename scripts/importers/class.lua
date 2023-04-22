d7a40b7df95a04241d0abdafc3e9fb341de00f0b
Josh LeBlanc <jleblanc@hey.com>
2023-04-22 08:52:39 -0300

    import class using charmanager

--- scripts/importers/class.lua
+++ scripts/importers/class.lua
@@ -1,19 +1,24 @@
-function import(node, value)
+function find_class(value)
   local classRecords = DB.getChildrenGlobal("reference.classes");
   for _, classNode in pairs(classRecords) do
-      local sRecordName = StringManager.trim(DB.getValue(classNode, "name", ""));
-      --print('Record name: ' .. sRecordName);
-      if (sRecordName:lower() == value:lower()) then
-          -- class found, set the class (NOTE: Only doing 1 class now)
-          --CharManager.addClass(nodeChar, "referenceclass", classNode.getNodeName());
-          local nodeList = node.createChild("classes");
-          nodeClass = nodeList.createChild();
-          DB.setValue(nodeClass, "name", "string", sRecordName);
-          DB.setValue(nodeClass, "level", "number", 1); -- TODO: Read Level
-          DB.setValue(nodeClass, "shortcut", "windowreference", "referenceclass", classNode.getNodeName());
-          break;
-      end
-      --error('Unable to find the background, make sure module is loaded');
-  
+    local sRecordName = StringManager.trim(DB.getValue(classNode, "name", ""));
+
+    if (sRecordName:lower() == value:lower()) then
+      return classNode
+    end
   end
+end
+
+function import(node, value)
+  local class = find_class(value)
+
+  if not class then return end
+
+  local nodeList = node.createChild("classes");
+  nodeClass = nodeList.createChild();
+
+  CharManager.addInfoDB(node, "referenceclass", class.getNodeName())
+
+  --DB.setValue(nodeClass, "name", "string", DB.getValue(class, "name", ""));
+  --DB.setValue(nodeClass, "shortcut", "windowreference", "referenceclass", class.getNodeName());
 end
