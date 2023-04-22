4f920ed32a7a97875d2bfe40ae586e93268e61d7
Josh LeBlanc <jleblanc@hey.com>
2023-04-22 08:52:05 -0300

    import background using charManager

--- scripts/importers/background.lua
+++ scripts/importers/background.lua
@@ -1,13 +1,18 @@
-function import(node, value)
-  local background = value
+function findBackground(value)
   local backgroundRecords = DB.getChildrenGlobal("reference.backgrounds")
   for _, backgroundNode in pairs(backgroundRecords) do
       local sRecordName = StringManager.trim(DB.getValue(backgroundNode, "name", ""))
 
       if (sRecordName:lower() == background:lower()) then
-          DB.setValue(node, "background", "string", sRecordName)
-          DB.setValue(node, "backgroundlink", "windowreference","reference_background", backgroundNode.getNodeName())
-          break
+        return backgroundNode
       end
   end
+end
+
+function import(node, value)
+  local background = findBackground(value)
+
+  if not background then return end 
+
+  CharManager.addInfoDB(node, "reference_background", background.getNodeName())
 end
