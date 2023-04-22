4c77d48e33113b80af3a90cd4a55a06508e18e9c
Josh LeBlanc <jleblanc@hey.com>
2023-04-22 15:11:44 -0300

    remove some logs

--- scripts/importers/feat.lua
+++ scripts/importers/feat.lua
@@ -13,10 +13,7 @@ end
 function isFeatDupe(nodeChar, nodeFeat)
   local featList = nodeChar.createChild("featlist");
 
-  Debug.console("FEAT LIST", featList.getChildren())
-
   for _, el in pairs(featList.getChildren()) do
-    Debug.chat(DB.getValue(el, "name", "") .. " == ", DB.getValue(nodeFeat, "name", ""))
     if DB.getValue(el, "name", "") == DB.getValue(nodeFeat, "name", "") then
 			return true;
 		end
@@ -33,14 +30,9 @@ end
 function import(node, value)
   local feat = findFeat(value[1])
 
-  Debug.chat("Importing " .. value[1])
-
   if not feat then return end 
 
-  if isFeatDupe(node, feat) then 
-    Debug.chat("Feat is duplicate " .. value[1])
-    return
-  end
+  if not isFeatDupe(node, feat) then return end
 
   CharManager.addInfoDB(node, "referencefeat", feat.getNodeName())
 end
