34ded854d402b57836cf237399cb34f61e76898b
Josh LeBlanc <jleblanc@hey.com>
2023-04-22 08:51:01 -0300

    import ancestry using char manager

--- scripts/importers/ancestry.lua
+++ scripts/importers/ancestry.lua
@@ -1,13 +1,19 @@
-function import(node, value)
+function findRace(value)
   local recordNodes = DB.getChildrenGlobal("reference.ancestries")
   for _, recordNode in pairs(recordNodes) do
       local sRecordName = StringManager.trim(DB.getValue(recordNode, "name", ""))
 
       if (sRecordName == value) then
-          -- ancestry found, set the ancestry
-          DB.setValue(node, "race", "string", sRecordName)
-          DB.setValue(node, "racelink", "windowreference", "referencerace", recordNode.getNodeName())
-          break
+          return recordNode
       end   
   end
+  
+end
+
+function import(node, value)
+  local race = findRace(value)
+
+  if not race then return end 
+
+  CharManager.addInfoDB(node, "referencerace", race.getNodeName())
 end
