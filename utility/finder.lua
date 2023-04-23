8157a9982e33edea988a5b513d227484df98f09a
Josh LeBlanc <jleblanc@hey.com>
2023-04-23 09:41:25 -0300

    simplify finder functions

--- utility/finder.lua
+++ utility/finder.lua
@@ -10,57 +10,45 @@ end
 function getLookupDataRecordGlobally(sRecordName, sLookupDataType)
 	local aDataMap = { "lookupdata", "reference.lookupdata" }
 
-  local sLookupDataType = fixString(sLookupDataType)
-  local sRecordName = fixString(sRecordName)
-	
-	local recordMatchNode = nil;
-	
+  sLookupDataType = fixString(sLookupDataType)
+  sRecordName = fixString(sRecordName)
+
 	for _, topLevelNodeName in pairs(aDataMap) do
 		local recordNodes = DB.getChildrenGlobal(topLevelNodeName)
 
 		for _, recordNode in pairs(recordNodes) do
-			if fixString(DB.getValue(recordNode, "lookupdatatype", "")) == sLookupDataType then
+      local recordCheckType = fixString(DB.getValue(recordNode, "lookupdatatype", ""))
+
+			if recordCheckType == sLookupDataType then
 				local recordCheckName = fixString(DB.getValue(recordNode, "name", ""))
+
 				if recordCheckName == sRecordName then
-					recordMatchNode = recordNode
-					break
+					return recordNode
 				end
 			end
 		end
-
-		if recordMatchNode ~= nil then break end
 	end
-	
-	return recordMatchNode;
 end
 
 function getRecordGlobally(sRecordName, aDataMap)
 	sRecordName = fixString(sRecordName)
 
-	-- Get record nodes from the database paths in aDataMap
-	local recordMatchNode = nil;
-	
-	for k, topLevelNodeName in pairs(aDataMap) do
-		local recordNodes = DB.getChildrenGlobal(topLevelNodeName); 
-		for k, recordNode in pairs(recordNodes) do
+	for _, topLevelNodeName in pairs(aDataMap) do
+		local recordNodes = DB.getChildrenGlobal(topLevelNodeName)
+
+		for _, recordNode in pairs(recordNodes) do
 			local recordCheckName = fixString(DB.getValue(recordNode, "name", ""))
+
 			if recordCheckName == sRecordName then
-				recordMatchNode = recordNode;
-				break;
+        return recordNode
 			end
-		end	
-		if recordMatchNode ~= nil then
-			break;
 		end
 	end
-	
-	return recordMatchNode;
 end
 
 function getFeat(sFeatName, sTrait)
-	if not sFeatName or sFeatName == "" then
-		return nil;	
-	end
+	if not sFeatName or sFeatName == "" then return end
+
 	if not sTrait then
 		sTrait = "";
 	end
@@ -68,32 +56,29 @@ function getFeat(sFeatName, sTrait)
   sFeatName = fixString(sFeatName)
   sTrait = fixString(sTrait)
 
-	local featMatchNode = nil;
 	local featNodes = DB.getChildrenGlobal("feat")
-	if featNodes then
-		for k, featNode in pairs(featNodes) do
-			local featCheckName = fixString(DB.getValue(featNode, "name", ""))
-			local featCheckTraits = fixString(DB.getValue(featNode, "traits", ""))
-			if featCheckName == sFeatName and string.find(featCheckTraits, sTrait) then
-				featMatchNode = featNode;
-				break;
-			end
-		end
-	end
-	
-	if featMatchNode == nil then
-		local featReferenceNodes = DB.getChildrenGlobal("reference.feats")
-		if featReferenceNodes then
-			for _,featNode in pairs(featReferenceNodes) do
-				local featCheckName = fixString(DB.getValue(featNode, "name", ""))
-				local featCheckTraits = fixString(DB.getValue(featNode, "traits", ""))		
-				if featCheckName == sFeatName and string.find(featCheckTraits, sTrait) then			
-					featMatchNode = featNode;
-					break
-				end
-			end	
-		end
-	end
-	
-	return featMatchNode
+
+  if not featNodes then return end
+
+  for _, featNode in pairs(featNodes) do
+    local featCheckName = fixString(DB.getValue(featNode, "name", ""))
+    local featCheckTraits = fixString(DB.getValue(featNode, "traits", ""))
+
+    if featCheckName == sFeatName and string.find(featCheckTraits, sTrait) then
+      return featNode
+    end
+  end
+
+  local featReferenceNodes = DB.getChildrenGlobal("reference.feats")
+
+  if not featReferenceNodes then return end
+
+  for _,featNode in pairs(featReferenceNodes) do
+    local featCheckName = fixString(DB.getValue(featNode, "name", ""))
+    local featCheckTraits = fixString(DB.getValue(featNode, "traits", ""))
+
+    if featCheckName == sFeatName and string.find(featCheckTraits, sTrait) then
+      return featNode
+    end
+  end
 end
