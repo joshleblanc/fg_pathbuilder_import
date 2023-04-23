4b2446c77936de47d60f58b3e14d81462203ac33
Josh LeBlanc <jleblanc@hey.com>
2023-04-23 08:54:04 -0300

    note feat params

--- scripts/importers/feat.lua
+++ scripts/importers/feat.lua
@@ -24,8 +24,13 @@ end
 
 --[[
   value is an array here
-  Looks like: [Name, ?, Type, ?]
+  Looks like: [Name, skill, Type, level attained]
 
+  Skill isn't used. It would be included if you picked Skill Training for example,
+  then [2] would be the skill you picked (eg. Intimidation). This'll be picked up in the
+  skill importer
+
+  [4] likewise isn't used. We don't care about the level
 --]]
 function import(node, value)
   local feat = findFeat(value[1])
