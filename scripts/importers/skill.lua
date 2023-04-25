a68a6b19ed61b291dc7554b5c17766a481a3d2be
Josh LeBlanc <jleblanc@hey.com>
2023-04-25 20:12:55 -0300

    don't import saves as skills

--- scripts/importers/skill.lua
+++ scripts/importers/skill.lua
@@ -1,11 +1,22 @@
+local saves = {
+  "will",
+  "fortitude",
+  "reflex"
+}
 
 -- CharManager.trainSkill(node, skillName)
 function import(node, value, key)
+  -- saves aren't skills 🙄
+  if StringManager.contains(saves, value) then 
+    return
+  end
+
   local skill = CharManager.getSkillNode(node, key)
 
   if not skill then 
     return key .. " not found"
   end
+
   if value == 2 then
     CharManager.setProficiency(skill, "trained")
   elseif value == 4 then 
