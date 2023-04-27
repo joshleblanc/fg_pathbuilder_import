407897b298da2fc5f97711efb0684952ae48ea2f
Josh LeBlanc <jleblanc@hey.com>
2023-04-27 20:41:40 -0300

    heck troubles in otari

--- scripts/importers/class.lua
+++ scripts/importers/class.lua
@@ -1,5 +1,16 @@
+local BAD_SOURCES = {
+  "Troubles in Otari Player's Resource" -- heck this book and it's level 4 classes
+}
+
 function find_class(value)
-  return Finder.getRecordGlobally(value, { "reference.classes" })
+  return Finder.getRecordGlobally(value, { "reference.classes" }, function(node)
+    
+    if StringManager.contains(BAD_SOURCES, node.getCategory()) then
+      return false
+    end
+
+    return true
+  end)
 end
 
 function import(node, value)
@@ -12,7 +23,4 @@ function import(node, value)
   CharManager.addInfoDB(node, "referenceclass", class.getNodeName())
 
   return value .. " imported as " .. DB.getValue(class, "name", "")
-
-  --DB.setValue(nodeClass, "name", "string", DB.getValue(class, "name", ""));
-  --DB.setValue(nodeClass, "shortcut", "windowreference", "referenceclass", class.getNodeName());
 end
