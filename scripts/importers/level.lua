16822d04d87788992ae904652fb6e20b7b188419
Josh LeBlanc <jleblanc@hey.com>
2023-04-29 07:13:20 -0300

    handle case where class isn't found

--- scripts/importers/level.lua
+++ scripts/importers/level.lua
@@ -1,5 +1,6 @@
 function import(node, value)
-  local curLevel = DB.getValue(node, "level", "")
+  local curLevel = DB.getValue(node, "level", 0)
+
   local numLevelsToAdd = value - curLevel
   for i=1,numLevelsToAdd do
     CharManager.addLevel(node)
